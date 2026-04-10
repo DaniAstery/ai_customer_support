f282c94025855ff0bbee5d7e49c6a605f22b5e16
Danieldoti <danizchrist@gmail.com>
2026-04-10 13:29:48 +0300

    testing verfiy token function

--- app.py
+++ app.py
@@ -111,10 +111,25 @@ User: {user_input}
 
 @app.route("/webhook", methods=["GET", "POST"])
 
+
+def verify():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge, 200
+    else:
+        return "Verification failed", 403
+
 def webhook():
 
-   if request.method == "POST":
-    data = request.get_json()
+    if request.method == 'GET':
+     return "Webhook verified", 200
+       
+    if request.method == "POST":
+    
+        data = request.get_json()
 
     print("🔥 WEBHOOK HIT!")
     print("FULL DATA:", data)
